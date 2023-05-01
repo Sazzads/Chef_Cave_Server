3c1110e64427ad219c1765c4fc92c66ca54a64a4
Sazzads <sazzadtk@gmail.com>
2023-05-02 02:14:23 +0600

    decleare port 500 and show on browser

--- index.js
+++ index.js
@@ -6,15 +6,7 @@ app.get('/', (req, res) => {
     res.json({ messege: "hello server running" })
 });
 
-const allChefs = require("./data/chefs.json")
-app.get('/allchef', (req, res) => {
-    res.json(allChefs)
-});
 
-const allfoods = require("./data/foods.json")
-app.get('/allfoods', (req, res) => {
-    res.json(allfoods)
-});
 
 app.listen(5000, () => {
     console.log("server is running on port 5000");
